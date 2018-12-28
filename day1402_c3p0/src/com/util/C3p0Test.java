f8493220382a6e94be7b12400698785acf30ee4e
ghity123 <841027513@qq.com>
2018-12-28 17:30:36 +0800

    tt

--- day1402_c3p0/src/com/util/C3p0Test.java
+++ day1402_c3p0/src/com/util/C3p0Test.java
@@ -10,7 +10,7 @@ public class C3p0Test {
 	
 	@Test
 	public void testSelect() throws SQLException{
-		//
+		System.out.println("ggg");
 		Connection conn=C3p0Util.getConnection();
 		PreparedStatement ps=null;
 		ps=conn.prepareStatement("insert into account values(4,100,'jimi')");
